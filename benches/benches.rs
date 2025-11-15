44ddc0951b86ba8490272a3f6aa6aa5d9609066d
lukaslueg <lukas.lueg@gmail.com>
2025-11-15 20:54:12 +0100

    Add benchmarks driven by criterion-rs

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,36 +1,82 @@
-#![feature(test)]
-extern crate test;
-use test::Bencher;
+use std::hint::black_box;
 
-fn inp(max: usize) -> Vec<impl std::hash::Hash + Eq> {
-    (0..max).map(|i| format!("Foo{}", i)).collect()
+fn bench_new(c: &mut criterion::Criterion) {
+    c.bench_function("Construct new sketch", |b| {
+        b.iter(|| black_box(hyperminhash::Sketch::default()))
+    });
 }
 
-macro_rules! make_bench {
-    ($name:ident, $count:expr) => {
-        mod $name {
-            #[bench]
-            fn count_hyperminhash(b: &mut super::Bencher) {
-                let inp = super::inp($count);
-                b.iter(|| {
-                    let sk = inp.iter().collect::<hyperminhash::Sketch>();
-                    test::black_box(sk.cardinality());
-                });
-            }
+fn bench_add(c: &mut criterion::Criterion) {
+    let plot_config =
+        criterion::PlotConfiguration::default().summary_scale(criterion::AxisScale::Logarithmic);
+    let mut group = c.benchmark_group("Adding small objects");
+    group.plot_config(plot_config);
+    for i in [10, 1_000, 100_000, 10_000_000] {
+        group.throughput(criterion::Throughput::Elements(i));
+        group.bench_with_input(
+            criterion::BenchmarkId::new("Add hashable object", i),
+            &i,
+            |b, i| b.iter(|| black_box((0..*i).collect::<hyperminhash::Sketch>())),
+        );
+    }
+    group.finish();
+}
 
-            #[bench]
-            fn count_hashset(b: &mut super::Bencher) {
-                let inp = super::inp($count);
-                b.iter(|| {
-                    let s = inp.iter().collect::<std::collections::HashSet<_>>();
-                    test::black_box(s.len());
-                });
-            }
+fn bench_cardinality(c: &mut criterion::Criterion) {
+    let sk = (0..100).collect::<hyperminhash::Sketch>();
+    c.bench_function("Determine cardinality", |b| {
+        b.iter(|| black_box(sk.cardinality()))
+    });
+}
+
+fn bench_similarity(c: &mut criterion::Criterion) {
+    let sk1 = (0..100).collect::<hyperminhash::Sketch>();
+    let sk2 = (50..150).collect::<hyperminhash::Sketch>();
+    c.bench_function("Determine similarity index", |b| {
+        b.iter(|| black_box(sk1.similarity(&sk2)))
+    });
+}
+
+fn bench_unique_integers(c: &mut criterion::Criterion) {
+    let plot_config =
+        criterion::PlotConfiguration::default().summary_scale(criterion::AxisScale::Logarithmic);
+    let mut group = c.benchmark_group("Counting unique integers");
+    group.plot_config(plot_config);
+    for i in [10, 1_000, 100_000, 10_000_000] {
+        if i > 1_000 {
+            group.sampling_mode(criterion::SamplingMode::Flat);
+            group.measurement_time(std::time::Duration::from_secs(10));
+        } else {
+            group.sampling_mode(criterion::SamplingMode::Linear);
+            group.measurement_time(std::time::Duration::from_secs(5));
         }
-    };
+        group.throughput(criterion::Throughput::Elements(i));
+        group.bench_with_input(
+            criterion::BenchmarkId::new("Hyperminhash", i),
+            &i,
+            |b, i| {
+                b.iter(|| {
+                    let sk = (0..*i).collect::<hyperminhash::Sketch>();
+                    black_box(sk.cardinality())
+                })
+            },
+        );
+        group.bench_with_input(criterion::BenchmarkId::new("HashSet", i), &i, |b, i| {
+            b.iter(|| {
+                let sk = (0..*i).collect::<std::collections::HashSet<_>>();
+                black_box(sk.len())
+            })
+        });
+    }
+    group.finish();
 }
 
-make_bench!(b01_ten, 10);
-make_bench!(b03_thousand, 1_000);
-make_bench!(b04_hundredthousand, 100_000);
-make_bench!(b05_million, 1_000_000);
+criterion::criterion_group!(
+    benches,
+    bench_new,
+    bench_cardinality,
+    bench_similarity,
+    bench_unique_integers,
+    bench_add
+);
+criterion::criterion_main!(benches);
